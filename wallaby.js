67cd7c6757f57fe5178ff29382e21c99ee09c8a8
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-07-23 03:37:04 +0200

    test: configure Wallaby and Jest

--- wallaby.js
+++ wallaby.js
@@ -1,26 +1,16 @@
-const compilerOptions = Object.assign(
-  {},
-  require('./tsconfig.json').compilerOptions,
-  require('./tsconfig.spec.json').compilerOptions,
-  { module: 'CommonJs' });
-
-module.exports = function (wallaby) {
+module.exports = function () {
   return {
-    compilers: {
-      '**/*.ts': wallaby.compilers.typeScript(compilerOptions)
-    },
-    debug: true,
-    env: {
-      kind: 'chrome',
-    },
     files: [
-      { pattern: 'src/lib/**/*.+(ts|json)', load: false },
-      { pattern: 'src/lib/**/*.d.ts', ignore: true },
-      { pattern: 'src/lib/**/*spec.ts', ignore: true },
+      'src/**/*.ts',
+      { pattern: 'src/**/*.spec.ts', ignore: true },
     ],
-    testFramework: 'jasmine',
     tests: [
-      'src/lib/**/*spec.ts',
+      'src/**/*.spec.ts',
     ],
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+    testFramework: 'jest'
   };
 };
